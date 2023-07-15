fbfd70e1d10f06b30332ca35cdef7272d17de99d
mafifanarss <sm.afifanaly@gmail.com>
2023-07-15 14:23:25 +0700

    migrasi ke laravel

---
+++ storage/framework/views/434e1e35dedaa744f3f1acb4be6274ee.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Mhd Afifan Aly Rahman Saragih\Teknologi Informasi '20\Competition\PKM 2023 - Scolar\Website\komikscolar.id\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
